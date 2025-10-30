91253e359c715c44e5b4b2035d1d160c57512b81
splittist <splittist@splittist.com>
2025-10-30 22:03:21 +0100

    Fix tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
